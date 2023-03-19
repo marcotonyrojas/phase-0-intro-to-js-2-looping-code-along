cf920cb1457b0aed9118415c4f18d565bb1d905f
Marco Ramirez <marco.ramirez.a@hotmail.com>
2023-03-19 02:18:14 -0500

    Loop Lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const names =["Me", "Myself","I"];
+const messages = [];
+function writeCards(names, eventNames){    
+    for (let i = 0; i < names.length; i ++){  
+    let message =  `Thank you, ${names[i]}, for the wonderful ${eventNames} gift!`; 
+    messages.push(message);} 
+return messages;
+}
+function countDown(number){    
+    while (number >= 0){        
+    console.log(number);        
+    number --;    
+}
+}
